7aac07da94c8802a707ef268cda184611b0ec280
devmashum <dev.mashum@gmail.com>
2023-09-30 23:17:37 +0200

    first commit

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
 const Register = () => {
+
+
     return (
         <div>
-            <h2 className="text-3xl">Please Register</h2>
+
+            <div className='mx-auto md:w-1/2'>
+                <h2 className=' text-3xl'>Please Register</h2>
+                <form className='mt-8'>
+                    <input className=' bg-blue-300 mb-4 py-2 px-4 w-full rounded-lg ' type="email" name="email" id="" placeholder='your email' />
+                    <br />
+                    <input className=' bg-blue-300 mb-4 py-2 px-4 w-full rounded-lg' type="password" name="password" id="" placeholder='your password' />
+                    <br />
+                    <input className='btn btn-secondary w-full' type="submit" value="Register" />
+                </form>
+            </div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import Root from './Components/Root/Root.jsx';
 import Home from './Components/Home/Home.jsx';
 import Login from './Components/Login/Login.jsx';
 import Register from './Components/Register/Register.jsx';
+import HeroRegister from './Components/Hero Register/HeroRegister.jsx';
 
 const router = createBrowserRouter([
   {
@@ -27,6 +28,11 @@ const router = createBrowserRouter([
       {
         path: '/register',
         element: <Register></Register>
+      },
+      {
+        path: '/heroregister',
+        element: <HeroRegister></HeroRegister>
+
       }
 
     ]
